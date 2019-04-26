f45f9aa40fa6745bf2b3584ac38d744815097800
Yan Shiqing <32927779+pudgeYsq1992@users.noreply.github.com>
2019-04-26 10:03:34 +0800

    '待标注数据已生成'

--- dataGen/dataGen.py
+++ dataGen/dataGen.py
@@ -16,12 +16,82 @@ def writeColName(worksheet):
     worksheet.write(1,4,'店家口味')
     worksheet.write(1,5,'菜价')
     worksheet.write(1,6,'距公司距离')
+    worksheet.write(1,8,'你有多想去这家店？')
     return
 
 def genRanddata(worksheet):
+    #生成“天气”一列
     for i in range(2,101):
-        if random.randint(1,4) = 1:
-            worksheet.write(i,1,'')
+        randomData = random.randint(0,3)
+        if randomData == 0:
+            worksheet.write(i,1,'恶劣')
+        if randomData == 1:
+            worksheet.write(i,1,'较差')
+        if randomData == 2:
+            worksheet.write(i,1,'较好')
+        if randomData == 3:
+            worksheet.write(i,1,'很好')
+    #生成“疲劳程度”一列   
+    for i in range(2,101):
+        randomData = random.randint(0,3)
+        if randomData == 0:
+            worksheet.write(i,2,'很累')
+        if randomData == 1:
+            worksheet.write(i,2,'有点累')
+        if randomData == 2:
+            worksheet.write(i,2,'不大累')
+        if randomData == 3:
+            worksheet.write(i,2,'很轻松')
+    #生成“周几”一列   
+    for i in range(2,101):
+        randomData = random.randint(0,6)
+        if randomData == 0:
+            worksheet.write(i,3,'星期一')
+        if randomData == 1:
+            worksheet.write(i,3,'星期二')
+        if randomData == 2:
+            worksheet.write(i,3,'星期三')
+        if randomData == 3:
+            worksheet.write(i,3,'星期四')
+        if randomData == 4:
+            worksheet.write(i,3,'星期五')
+        if randomData == 5:
+            worksheet.write(i,3,'星期六')
+        if randomData == 6:
+            worksheet.write(i,3,'星期天')
+    #生成“店家口味”一列   
+    for i in range(2,101):
+        randomData = random.randint(0,3)
+        if randomData == 0:
+            worksheet.write(i,4,'不好吃')
+        if randomData == 1:
+            worksheet.write(i,4,'一般般')
+        if randomData == 2:
+            worksheet.write(i,4,'还不错')
+        if randomData == 3:
+            worksheet.write(i,4,'很好吃')
+    #生成“菜价”一列   
+    for i in range(2,101):
+        randomData = random.randint(0,3)
+        if randomData == 0:
+            worksheet.write(i,5,'便宜')
+        if randomData == 1:
+            worksheet.write(i,5,'还行')
+        if randomData == 2:
+            worksheet.write(i,5,'有点贵')
+        if randomData == 3:
+            worksheet.write(i,5,'很贵')
+    #生成“距离”一列   
+    for i in range(2,101):
+        randomData = random.randint(0,3)
+        if randomData == 0:
+            worksheet.write(i,6,'很远')
+        if randomData == 1:
+            worksheet.write(i,6,'有些远')
+        if randomData == 2:
+            worksheet.write(i,6,'不算远')
+        if randomData == 3:
+            worksheet.write(i,6,'很近')
     return
     
 def dataGen():
